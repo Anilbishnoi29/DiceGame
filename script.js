8dc7c027a717618ffd184210de29cda50b1190cf
Anilbishnoi29 <bishnoianilakb@gmail.com>
2022-02-23 23:59:03 +0530

    first commit

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 'use strict';
 // Selecting Elements
 const getName = document.getElementById('getName');
+// input name
+const playerName0 = document.getElementById('playerName--0');
+const playerName1 = document.getElementById('playerName--1');
 
 // overlay
 getName.addEventListener('click', function () {
+  // set player name
+  document.getElementById('name--0').textContent = playerName0.value;
+  document.getElementById('name--1').textContent = playerName1.value;
+  // hide overlay
   document.getElementById('overlay').classList.add('d-translate');
   setTimeout(function () {
     document.getElementById('overlay').classList.add('d-none');
